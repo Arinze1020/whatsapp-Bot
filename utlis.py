35d38cb6bdceff1699fa422e7fa8e72f35d786c9
Arinze ugwuanyi <arinzeugwuanyi@gmail.com>
2019-10-17 00:06:08 +0100

    update

--- utlis.py
+++ utlis.py
@@ -1,18 +1,24 @@
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "phishing-detection-200912-9a24f78c0df5.json"
 
+os.environ[
+    "GOOGLE_APPLICATION_CREDENTIALS"
+] = "phishing-detection-200912-9a24f78c0df5.json"
 import dialogflow_v2 as dialogflow
+
 dialogflow_session_client = dialogflow.SessionsClient()
 PROJECT_ID = "phishing-detection-200912"
 
-def detect_intent_from_text(text, session_id, language_code='en'):
+
+def detect_intent_from_text(text, session_id, language_code="en"):
     session = dialogflow_session_client.session_path(PROJECT_ID, session_id)
     text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
     query_input = dialogflow.types.QueryInput(text=text_input)
-    response = dialogflow_session_client.detect_intent(session=session, query_input=query_input)
+    response = dialogflow_session_client.detect_intent(
+        session=session, query_input=query_input
+    )
     return response.query_result
 
 
 def fetch_reply(query, session_id):
-	response = detect_intent_from_text(query,session_id)
-	return response.fulfillment_text
+    response = detect_intent_from_text(query, session_id)
+    return response.fulfillment_text
